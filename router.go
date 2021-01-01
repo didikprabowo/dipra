2fed751f6413fe9d1dc5f2a6e8ee274415a86ad3
didikprabowo <didikprab@gmail.com>
2021-01-02 00:03:07 +0700

    handle route and remove cache routes

---
+++ router.go
@@ -0,0 +1,78 @@
+package dipra
+
+import (
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+var (
+	// List method is allowed
+	allowMethod = []string{
+		http.MethodGet,
+		http.MethodDelete,
+		http.MethodPost,
+		http.MethodConnect,
+		http.MethodPatch,
+		http.MethodPut,
+	}
+)
+
+// allowMethods for use check your define a method
+func (e *Engine) allowMethods(m string) (ok bool) {
+	for _, mlist := range allowMethod {
+		if mlist == m {
+			return true
+		}
+	}
+	return ok
+}
+
+// allowPath for use check your path
+func (e *Engine) allowPath(p string) (ok bool) {
+
+	if strings.HasPrefix(p, "/") {
+		p = p[1 : len(p)-1]
+	}
+
+	isAllow := regexp.MustCompile(`[a-zA-Z0-9]$`)
+	ok = isAllow.MatchString(p)
+
+	return ok
+}
+
+/*
+	findRouter for used to checking existing path,
+	method and handler
+*/
+func (e *Engine) findRouter(m, p string, h HandlerFunc) (exist bool) {
+
+	if ok := e.allowMethods(m); !ok {
+		log.Printf("Method %s not allow", m)
+		return
+	}
+
+	if ok := e.allowPath(p); !ok {
+		log.Printf("Path %s is invalid syntax", p)
+		return
+	}
+
+	toLower := func(s string) string {
+		return strings.ToLower(s)
+	}
+
+	for i := range e.getRoutes() {
+		if toLower(e.Route[i].Method) == toLower(m) &&
+			toLower(e.Route[i].Path) == toLower(p) {
+			e.Route[i].Handler = h
+			return true
+		}
+	}
+	return exist
+}
+
+// getRoutes for get The routes you define
+func (e *Engine) getRoutes() []Route {
+	return e.Route
+}
